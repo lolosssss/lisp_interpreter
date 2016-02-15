dfae48a28820448310bed7031ca5fa5d4adee9f0
lolosssss <814364672@qq.com>
2016-02-15 09:41:30 +0800

    add Input Type to mpc

--- mpc.c
+++ mpc.c
@@ -311,3 +311,408 @@ static mpc_err_t *mpc_err_count(mpc_err_t *x, int n)
 
     return y;
 }
+
+// Input Type
+
+/*
+ * In mpc the input type has three modes of
+ * operation: String, File and Pipe.
+ *
+ * String is easy. The whole contents are loaded
+ * into a buffer and scanned through. The cursor
+ * can jump around at will makeing backtracking
+ * easy.
+ *
+ * The second is a File which is also somewhat
+ * easy. The contents are never loaded into
+ * memory but backtracking can still be achieved
+ * by seeking in the file at different positions.
+ *
+ * The final mode is Pipe. This is the difficult
+ * one. As we assume pipes cannot be seeked - and
+ * only support a single character lookahead at
+ * any point, when the input is marked for a
+ * potential backtracking we start buffering any
+ * input.
+ *
+ * This means that if we are requested to seek
+ * back we can simply start reading from the
+ * buffer instead of the input.
+ *
+ * Of course using `mpc_predictive` will disable
+ * backtracking and make LL(1) grammars easy
+ * to parse for all input methods.
+ *
+ */
+
+static mpc_input_t *mpc_input_new_string(const char *filename, const char *string)
+{
+    mpc_input_t *i = malloc(sizeof(mpc_input_t));
+
+    i->filename = malloc(strlen(filename) + 1);
+    strcpy(i->filename, filename);
+    i->type = MPC_INPUT_STRING;
+
+    i->state = mpc_state_new();
+
+    i->string = malloc(strlen(string) + 1);
+    strcpy(i->string, string);
+    i->buffer = NULL;
+    i->file = NULL;
+
+    i->backtrack = 1;
+    i->marks_num = 0;
+    i->marks = NULL;
+    i->lasts = NULL;
+
+    i->last = '\0';
+
+    return i;
+}
+static mpc_input_t *mpc_input_new_pipe(const char *filename, FILE *pipe)
+{
+    mpc_input_t *i = malloc(sizeof(mpc_input_t));
+
+    i->filename = malloc(strlen(filename) + 1);
+    strcpy(i->filename, filename);
+
+    i->type = MPC_INPUT_PIPE;
+    i->state = mpc_state_new();
+
+    i->string = NULL;
+    i->buffer = NULL;
+    i->file = pipe;
+
+    i->backtrack = 1;
+    i->marks_num = 0;
+    i->marks = NULL;
+    i->lasts = NULL;
+
+    i->last = '\0';
+
+    return i;
+}
+
+static mpc_input_t *mpc_input_new_file(const char *filename, FILE *file)
+{
+    mpc_input_t *i = malloc(sizeof(mpc_input_t));
+
+    i->filename = malloc(strlen(filename) + 1);
+    strcpy(i->filename, filename);
+    i->type = MPC_INPUT_FILE;
+    i->state = mpc_state_new();
+
+    i->string = NULL;
+    i->buffer = NULL;
+    i->file = file;
+
+    i->backtrack = 1;
+    i->marks_num = 0;
+    i->marks = NULL;
+    i->lasts = NULL;
+
+    i->last = '\0';
+
+    return i;
+}
+
+static void mpc_input_delete(mpc_input_t *i)
+{
+    free(i->filename);
+
+    if (i->type == MPC_INPUT_STRING)
+        free(i->string);
+    if (i->type == MPC_INPUT_PIPE)
+        free(i->buffer);
+
+    free(i->marks);
+    free(i->lasts);
+    free(i);
+}
+
+static void mpc_input_backtrack_disable(mpc_input_t *i)
+{
+    i->backtrack--;
+}
+
+static void mpc_input_backtrack_enable(mpc_input_t *i)
+{
+    i->backtrack++;
+}
+
+static void mpc_input_mark(mpc_input_t *i)
+{
+    if (i->backtrack < 1)
+        return;
+
+    i->marks_num++;
+    i->marks = realloc(i->marks, sizeof(mpc_state_t) * i->marks_num);
+    i->lasts = realloc(i->lasts, sizeof(char) * i->marks_num);
+    i->marks[i->marks_num - 1] = i->state;
+    i->lasts[i->marks_num - 1] = i->last;
+
+    if (i->type == MPC_INPUT_PIPE && i->marks_num == 1)
+    {
+        i->buffer = calloc(1, 1);
+    }
+}
+
+static void mpc_input_unmark(mpc_input_t *i)
+{
+    if (i->backtrack < 1)
+        return;
+
+    i->marks_num--;
+    i->marks = realloc(i->marks, sizeof(mpc_state_t) * i->marks_num);
+    i->lasts = realloc(i->lasts, sizeof(char) * i->marks_num);
+
+    if (i->type == MPC_INPUT_PIPE && i->marks_num == 0)
+    {
+        free(i->buffer);
+        i->buffer = NULL;
+    }
+}
+
+static void mpc_input_rewind(mpc_input_t *i)
+{
+    if (i->backtrack < 1)
+        return;
+
+    i->state = i->marks[i->marks_num - 1];
+    i->last = i->lasts[i->marks_num - 1];
+
+    if (i->type == MPC_INPUT_FILE)
+    {
+        fseek(i->file, i->state.pos, SEEK_SET);
+    }
+
+    mpc_input_unmark(i);
+}
+
+static int mpc_input_buffer_in_range(mpc_input_t *i)
+{
+    return i->state.pos < (strlen(i->buffer) + i->marks[0].pos);
+}
+
+static char mpc_input_buffer_get(mpc_input_t *i)
+{
+    return i->buffer[i->state.pos - i->marks[0].pos];
+}
+
+static int mpc_input_terminated(mpc_input_t *i)
+{
+    if (i->type == MPC_INPUT_STRING && i->state.pos == strlen(i->string))
+        return 1;
+    if (i->type == MPC_INPUT_FILE && feof(i->file))
+        return 1;
+    if (i->type == MPC_INPUT_PIPE && feof(i->file))
+        return 1;
+
+    return 0;
+}
+
+static char mpc_input_getc(mpc_input_t *i)
+{
+    char c = '\0';
+
+    switch (i->type)
+    {
+        case MPC_INPUT_STRING: return i->string[i->state.pos];
+        case MPC_INPUT_FILE:   c = fgetc(i->file); return c;
+        case MPC_INPUT_PIPE:
+            {
+                if (!i->buffer)
+                {
+                    c = getc(i->file);
+                    return c;
+                }
+                if (i->buffer && mpc_input_buffer_in_range(i))
+                {
+                    c = mpc_input_buffer_get(i);
+                    return c;
+                }
+                else
+                {
+                    c = getc(i->file);
+                    return c;
+                }
+            }
+        default: return c;
+    }
+}
+
+static char mpc_input_peekc(mpc_input_t *i)
+{
+    char c = '\0';
+
+    switch (i->type)
+    {
+        case MPC_INPUT_STRING: return i->string[i->state.pos];
+        case MPC_INPUT_FILE:
+            c = fgetc(i->file);
+            if (feof(i->file))
+                return '\0';
+            fseek(i->file, -1, SEEK_CUR);
+            return c;
+        case MPC_INPUT_PIPE:
+            if (!i->buffer)
+            {
+                c = getc(i->file);
+                if (feof(i->file))
+                    return '\0';
+                ungetc(c, i->file);
+                return c;
+            }
+            if (i->buffer && mpc_input_buffer_in_range(i))
+            {
+                return mpc_input_buffer_get(i);
+            }
+            else
+            {
+                c = getc(i->file);
+                if (feof(i->file))
+                    return '\0';
+                ungetc(c, i->file);
+                return c;
+            }
+        default:
+            return c;
+    }
+}
+
+static int mpc_input_failure(mpc_input_t *i, char c)
+{
+    switch (i->type)
+    {
+        case MPC_INPUT_STRING:
+            break;
+        case MPC_INPUT_FILE:
+            fseek(i->file, -1, SEEK_CUR);
+            break;
+        case MPC_INPUT_PIPE:
+            if (!i->buffer)
+            {
+                ungetc(c, i->file);
+                break;
+            }
+            if (i->buffer && mpc_input_buffer_in_range(i))
+                break;
+            else
+                ungetc(c, i->file);
+    }
+
+    return 0;
+}
+
+static int mpc_input_success(mpc_input_t *i, char c, char **o)
+{
+    if (i->type == MPC_INPUT_PIPE && i->buffer && !mpc_input_buffer_in_range(i))
+    {
+        i->buffer = realloc(i->buffer, strlen(i->buffer) + 2);
+        i->buffer[strlen(i->buffer) + 1] = '\0';
+        i->buffer[strlen(i->buffer) + 0] = c;
+    }
+
+    i->last = c;
+    i->state.pos++;
+    i->state.col++;
+
+    if (c == '\n')
+    {
+        i->state.col = 0;
+        i->state.row++;
+    }
+
+    if (o)
+    {
+        (*o) = malloc(2);
+        (*o)[0] = c;
+        (*o)[1] = '\0';
+    }
+
+    return 1;
+}
+
+static int mpc_input_any(mpc_input_t *i, char **o)
+{
+    char x = mpc_input_getc(i);
+    if (mpc_input_terminated(i))
+        return 0;
+
+    return mpc_input_success(i, x, o);
+}
+
+static int mpc_input_char(mpc_input_t *i, char c, char **o)
+{
+    char x = mpc_input_getc(i);
+    if (mpc_input_terminated(i))
+        return 0;
+
+    return x == c ? mpc_input_success(i, x, o) : mpc_input_failure(i, x);
+}
+static int mpc_input_range(mpc_input_t *i, char c, char d, char **o)
+{
+    char x = mpc_input_getc(i);
+    if (mpc_input_terminated(i))
+        return 0;
+
+    return x >= c && x <= d ? mpc_input_success(i, x, o) : mpc_input_failure(, x);
+}
+
+static int mpc_input_oneof(mpc_input_t *i, const char *c, char **o)
+{
+    char x = mpc_input_getc(i);
+    if (mpc_input_terminated(i))
+        return 0;
+
+    return strchr(c, x) != 0 ? mpc_input_success(i, x, o) : mpc_input_failure(i, x);
+}
+
+static int mpc_input_noneof(mpc_input_t *i, const char *c, char **o)
+{
+    char x = mpc_input_getc(i);
+    if (mpc_input_terminated(i))
+        return 0;
+
+    return strchr(c, x) == 0 ? mpc_input_success(i, x, o) : mpc_input_failure(i, x);
+}
+
+static int mpc_input_satisfy(mpc_input_t *i, int(*cond)(char), char **o)
+{
+    char x = mpc_input_getc(i);
+    if (mpc_input_terminated(i))
+        return 0;
+
+    return cond(x) ? mpc_input_success(i, x, o) : mpc_input_failure(i, x);
+}
+
+static int mpc_input_string(mpc_input_t *i, const char *c, char **o)
+{
+    char *co = NULL;
+    const char *x = c;
+
+    mpc_input_mark(i);
+    while (*x)
+    {
+        if (mpc_input_char(i, *x, &co))
+            free(co);
+        else
+        {
+            mpc_input_rewind(i);
+            return 0;
+        }
+
+        x++;
+    }
+    mpc_input_unmark(i);
+
+    *o = malloc(strlen(c) + 1);
+    strcpy(*o, c);
+
+    return 1;
+}
+
+static int mpc_input_anchor(mpc_input_t *i, int (*f)(char, char))
+{
+    return f(i->last, mpc_input_peekc(i));
+}
--- mpc.h
+++ mpc.h
@@ -25,23 +25,46 @@ typedef struct {
 typedef struct {
     mpc_state_t state;
     int expected_num;
-    char* filename;
-    char* failure;
-    char* *expected;
+    char *filename;
+    char *failure;
+    char **expected;
     char recieved;
 } mpc_err_t;
 
-void mpc_err_delete(mpc_err_t* e);
-char* mpc_err_string(mpc_err_t* e);
-void mpc_err_print(mpc_err_t* e);
-void mpc_err_print_to(mpc_err_t* e, FILE* f);
+void mpc_err_delete(mpc_err_t *e);
+char* mpc_err_string(mpc_err_t *e);
+void mpc_err_print(mpc_err_t *e);
+void mpc_err_print_to(mpc_err_t *e, FILE *f);
+
+// Input Type
+enum {
+    MPC_INPUT_STRING = 0,
+    MPC_INPUT_FILE = 1,
+    MPC_INPUT_PIPE = 2
+};
+
+typedef struct {
+    int type;
+    char *filename;
+    mpc_state_t state;
+
+    char *string;
+    char *buffer;
+    FILE *file;
+
+    int backtrack;
+    int marks_num;
+    mpc_state_t *marks;
+    char *lasts;
+    char last;
+} mpc_input_t;
 
 // Parsing
 typedef void mpc_val_t;
 
 typedef union {
-    mpc_err_t* error;
-    mpc_val_t* output;
+    mpc_err_t *error;
+    mpc_val_t *output;
 } mpc_result_t;
 
 struct mpc_parser_t;

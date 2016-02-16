9c4e8c75803f39c12beee4cd1e2be20391001446
lolosssss <814364672@qq.com>
2016-02-16 12:45:49 +0800

    add parser type

--- mpc.h
+++ mpc.h
@@ -21,6 +21,7 @@ typedef struct {
     int col;
 } mpc_state_t;
 
+
 // Error Type
 typedef struct {
     mpc_state_t state;
@@ -35,6 +36,7 @@ void mpc_err_delete(mpc_err_t *e);
 char* mpc_err_string(mpc_err_t *e);
 void mpc_err_print(mpc_err_t *e);
 void mpc_err_print_to(mpc_err_t *e, FILE *f);
+
 
 // Input Type
 enum {
@@ -59,6 +61,7 @@ typedef struct {
     char last;
 } mpc_input_t;
 
+
 // Parsing
 typedef void mpc_val_t;
 
@@ -69,5 +72,27 @@ typedef union {
 
 struct mpc_parser_t;
 typedef struct mpc_parser_t mpc_parser_t;
+
+int mpc_parse(const char *filename, const char *string, mpc_parser_t *p, mpc_result_t *r);
+int mpc_parse_file(const char *filename, FILE *file, mpc_parser_t *p, mpc_result_t *r);
+int mpc_parse_pipe(const char *filename, FILE *pipe, mpc_parser_t *p, mpc_result_t *r);
+int mpc_parse_contents(const char *filename, mpc_parser_t *p, mpc_result_t *r);
+
+
+// Function Types
+typedef void (*mpc_dtor_t)(mpc_val_t *);
+typedef mpc_val_t *(*mpc_ctor_t)(void);
+typedef mpc_val_t *(*mpc_apply_t)(mpc_val_t *, void *);
+typedef mpc_val_t *(*mpc_apply_to_t)(mpc_val_t *, void *);
+typedef mpc_val_t *(*mpc_fold_t)(int, mpc_val_t **);
+
+
+// Building a Parser
+mpc_parser_t *mpc_new(const char *name);
+mpc_parser_t *mpc_define(mpc_parser_t *p, mpc_parser_t *a);
+mpc_parser_t *mpc_undefine(mpc_parser_t *p);
+
+void mpc_delete(mpc_parser_t *p);
+void mpc_cleanup(int n, ...);
 
 #endif  // __MPC_H
